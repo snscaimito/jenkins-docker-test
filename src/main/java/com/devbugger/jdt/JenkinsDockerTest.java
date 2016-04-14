89d27c4ac4aea51ee95ce409e2983a9db195f5ce
Dag Østgulen Heradstveit <dag.heradstveit@nsd.uib.no>
2016-04-14 15:47:10 +0200

    Migrated to spring boot mvc to add more real value

--- src/main/java/com/devbugger/jdt/JenkinsDockerTest.java
+++ src/main/java/com/devbugger/jdt/JenkinsDockerTest.java
@@ -1,11 +1,12 @@
 package com.devbugger.jdt;
 
-public class JenkinsDockerTest {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@SpringBootApplication
+public class JenkinsDockerTest {
     public static void main(String[] args) {
-    System.out.println("Hello from inside an automatically built docker conainer.\n" +
-                "Lets find the mid sum of 100 to 5");
-        Calc c = new Calc();
-        System.out.println(c.calc(100, 5));
+        ApplicationContext ctx = SpringApplication.run(JenkinsDockerTest.class);
     }
 }
